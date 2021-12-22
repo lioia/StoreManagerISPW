21f3135bc4f64f00be8ae7d8f6e4f27c773c8650
Alessandro Lioi <lioialessandro@users.noreply.github.com>
2021-12-22 16:48:24 +0100

    Close #17

--- ShoppingPoint/src/main/java/com/example/shoppingpoint/view/RegisterGraphicController.java
+++ ShoppingPoint/src/main/java/com/example/shoppingpoint/view/RegisterGraphicController.java
@@ -1,4 +1,18 @@
 package com.example.shoppingpoint.view;
 
+import com.example.shoppingpoint.ShoppingPointApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+
+import java.io.IOException;
+
 public class RegisterGraphicController {
+
+    @FXML
+    protected void goBack(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ShoppingPointApplication.class.getResource("login.fxml"));
+        ((Node) event.getSource()).getScene().setRoot(fxmlLoader.load());
+    }
 }
--- ShoppingPoint/src/main/java/module-info.java
+++ ShoppingPoint/src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module com.example.shoppingpoint {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.material2;
 
     opens com.example.shoppingpoint to javafx.fxml;
     opens com.example.shoppingpoint.view to javafx.fxml;
