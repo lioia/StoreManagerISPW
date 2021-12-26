d5563bff2e960df641a6fb110ba995e3ab368f56
GRonz00 <92393572+GRonz00@users.noreply.github.com>
2021-12-26 21:04:20 +0100

    Close #30

---
+++ ShoppingPoint/src/main/java/com/example/shoppingpoint/view/EditAccountGraphicController.java
@@ -0,0 +1,12 @@
+package com.example.shoppingpoint.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+public class EditAccountGraphicController {
+}
