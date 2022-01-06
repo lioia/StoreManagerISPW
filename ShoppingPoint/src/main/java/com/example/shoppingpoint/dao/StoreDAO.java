7fa88342315177f94a95dc8c4eaddb888c4f97af
Alessandro Lioi <lioialessandro@users.noreply.github.com>
2022-01-06 11:59:59 +0100

    Close #38

--- ShoppingPoint/src/main/java/com/example/shoppingpoint/dao/StoreDAO.java
+++ ShoppingPoint/src/main/java/com/example/shoppingpoint/dao/StoreDAO.java
@@ -9,11 +9,54 @@ import com.example.shoppingpoint.utils.StoreType;
 import com.example.shoppingpoint.utils.UserType;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StoreDAO {
     private StoreDAO() {
         throw new IllegalStateException();
     }
+
+    public static List<Store> getAllStores() throws Exception {
+        Statement statement = null;
+        Connection connection = null;
+        List<Store> stores = new ArrayList<Store>();
+
+        try {
+            // Create Connection
+            connection = DriverManager.getConnection(Database.DB_URL, Database.USER, Database.PASS);
+            // Create statement
+            statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            // Execute query
+            ResultSet rs = statement.executeQuery("SELECT * FROM Store");
+            while (!rs.next()) {
+                String name = rs.getString("Name");
+                String address = rs.getString("Address");
+                Integer pointsInEuro = rs.getInt("PointsInEuro");
+                StoreType type = StoreType.valueOf(rs.getString("Type"));
+                Store store = new Store(name, address, pointsInEuro, type);
+                stores.add(store);
+            }
+
+            rs.close();
+        } finally {
+            // Clean-up dell'ambiente
+            try {
+                if (statement != null)
+                    statement.close();
+            } catch (SQLException se2) {
+                se2.printStackTrace();
+            }
+            try {
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        return stores;
+    }
+
 
     public static Store getStoreByName(String name) throws Exception {
         Statement statement = null;
