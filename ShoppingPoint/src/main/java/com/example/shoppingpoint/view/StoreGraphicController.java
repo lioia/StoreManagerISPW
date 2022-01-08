0dae7a29adefda4f6cada451abdb7f4cc4ade09e
Alessandro Lioi <lioialessandro@users.noreply.github.com>
2022-01-08 18:19:28 +0100

    Close #21

---
+++ ShoppingPoint/src/main/java/com/example/shoppingpoint/view/StoreGraphicController.java
@@ -0,0 +1,15 @@
+package com.example.shoppingpoint.view;
+
+import com.example.shoppingpoint.model.Store;
+
+public class StoreGraphicController {
+    private Store store;
+
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
+}
