bd4e0c8585eb25dae93fcfa439400af9f69b5071
Alessandro Lioi <lioialessandro@users.noreply.github.com>
2022-01-21 17:48:30 +0100

    Close #49

--- ShoppingPoint/src/main/java/com/example/shoppingpoint/view/OrdersGraphicController.java
+++ ShoppingPoint/src/main/java/com/example/shoppingpoint/view/OrdersGraphicController.java
@@ -10,12 +10,19 @@ import com.example.shoppingpoint.model.user.Client;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
+import org.controlsfx.control.PopOver;
 import org.controlsfx.control.Rating;
+import org.kordamp.ikonli.javafx.FontIcon;
 
 import java.util.List;
 
@@ -64,5 +71,34 @@ public class OrdersGraphicController {
         ((Node)actionEvent.getSource()).getScene().setRoot(node);
         StoreGraphicController storeGraphicController = loader.getController();
         storeGraphicController.initData(store, client);
+    }
+
+    @FXML
+    public void openAccountInfo(ActionEvent actionEvent) {
+        VBox vbox = new VBox(16);
+        vbox.setPadding(new Insets(16));
+        Button logoutButton = new Button("Log Out");
+        logoutButton.setAlignment(Pos.CENTER);
+        logoutButton.setStyle("-fx-background-color: #6EC6FF; -fx-background-radius: 16;");
+        logoutButton.setOnAction(event -> {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(ShoppingPointApplication.class.getResource("login.fxml"));
+                ((Node) actionEvent.getSource()).getScene().setRoot(fxmlLoader.load());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        logoutButton.setEffect(new DropShadow());
+        logoutButton.setGraphic(new FontIcon("mdal-log_out"));
+        logoutButton.setPrefSize(120, 48);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.getChildren().add(logoutButton);
+
+        PopOver popOver = new PopOver();
+        Node node = (Node) actionEvent.getSource();
+        popOver.setArrowLocation(PopOver.ArrowLocation.TOP_CENTER);
+        popOver.setContentNode(vbox);
+        popOver.setCornerRadius(16);
+        popOver.show(node);
     }
 }
