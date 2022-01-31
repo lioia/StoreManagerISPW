22f87ab67b64b21f912616cb59df7530c9a2a274
Alessandro Lioi <lioialessandro@users.noreply.github.com>
2022-01-31 11:53:25 +0100

    Close #58

--- ShoppingPoint/src/main/java/com/example/shoppingpoint/bean/RegisterBean.java
+++ ShoppingPoint/src/main/java/com/example/shoppingpoint/bean/RegisterBean.java
@@ -1,6 +1,8 @@
 package com.example.shoppingpoint.bean;
 
+import com.example.shoppingpoint.exception.BeanException;
 import com.example.shoppingpoint.utils.UserType;
+
 import java.util.regex.*;
 
 public class RegisterBean {
@@ -9,62 +11,63 @@ public class RegisterBean {
     private String password;
     private UserType userType;
 
-    public RegisterBean() {}
-
-    public  RegisterBean(String email, String username, String userType, String password, String verifyPassword) {
+    public RegisterBean(String email, String username, String userType, String password, String verifyPassword) throws BeanException {
         setEmail(email);
         setUsername(username);
         setUserType(userType);
         setPassword(password);
-        setVerifyPassword(password,verifyPassword);
+        setVerifyPassword(password, verifyPassword);
     }
-    public String getEmail(){return  email;}
-    public String getUsername(){ return username;}
-    public UserType getUserType(){ return userType;}
-    public String getPassword(){ return password;}
 
+    public String getEmail() {
+        return email;
+    }
 
-    // TODO: bean exception
-    public void setUsername(String user) {
-        if (user.length() < 4 || user.length() > 16) {
-//            TODO throws exception
-            System.out.println("Username not valid");
-        }
+    public String getUsername() {
+        return username;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String user) throws BeanException {
+        if (user.length() < 4) throw new BeanException("username", "too short");
+        if (user.length() > 16) throw new BeanException("username", "too long");
         this.username = user;
     }
 
-    public void setPassword(String pass) {
-        if(pass.length() < 8 || pass.length() > 16) {
-//            TODO throws exception
-            System.out.println("Password not valid");
-        }
+    public void setPassword(String pass) throws BeanException {
+        if (pass.length() < 8) throw new BeanException("password", "too short");
+        if (pass.length() > 16) throw new BeanException("password", "too long");
         this.password = pass;
     }
 
-    public void setVerifyPassword(String pass,String verifyPassword) {
-        if(!pass.equals(verifyPassword)) {
-//            TODO throws exception
-            System.out.println("VerifyPassword not valid");
-        }
+    public void setVerifyPassword(String pass, String verifyPassword) throws BeanException {
+        if (!pass.equals(verifyPassword)) throw new BeanException("verify password", "not equal to password");
     }
 
-    public void  setEmail(String email) {
+    public void setEmail(String email) throws BeanException {
         String regex = "[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}";
 
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
-        if(!matcher.matches()){
-            //            TODO throws exception
-            System.out.println("Email not valid");
-        }
-        this.email= email;
+        if (!matcher.matches())
+            throw new BeanException("email", "not valid");
+
+        this.email = email;
     }
 
-    public void setUserType(String type){
+    public void setUserType(String type) {
         switch (type) {
+            case "Client" -> this.userType = UserType.CLIENT;
             case "Store Owner" -> this.userType = UserType.STOREOWNER;
             case "Supplier" -> this.userType = UserType.SUPPLIER;
-            default -> this.userType = UserType.CLIENT; // case "Client"
+            default -> throw new IllegalStateException("Unexpected value: " + type);
         }
     }
 }
--- ShoppingPoint/src/main/java/com/example/shoppingpoint/view/RegisterGraphicController.java
+++ ShoppingPoint/src/main/java/com/example/shoppingpoint/view/RegisterGraphicController.java
@@ -3,6 +3,7 @@ package com.example.shoppingpoint.view;
 import com.example.shoppingpoint.ShoppingPointApplication;
 import com.example.shoppingpoint.bean.RegisterBean;
 import com.example.shoppingpoint.controller.RegisterController;
+import com.example.shoppingpoint.exception.BeanException;
 import com.example.shoppingpoint.model.user.Client;
 import com.example.shoppingpoint.model.user.StoreOwner;
 import com.example.shoppingpoint.model.user.Supplier;
@@ -13,6 +14,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ComboBox;
@@ -20,8 +22,6 @@ import javafx.scene.control.ComboBox;
 import java.io.IOException;
 
 public class RegisterGraphicController {
-
-    RegisterController controller;
 
     @FXML
     private TextField usernameTextField;
@@ -35,40 +35,61 @@ public class RegisterGraphicController {
     private ComboBox<String> userTypeField;
 
     @FXML
-    protected void goBack(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ShoppingPointApplication.class.getResource("login.fxml"));
-        ((Node) event.getSource()).getScene().setRoot(fxmlLoader.load());
+    protected void goBack(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(ShoppingPointApplication.class.getResource("login.fxml"));
+            ((Node) event.getSource()).getScene().setRoot(fxmlLoader.load());
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("Load error");
+            alert.setContentText("Could not load the next screen. Please try again");
+            alert.show();
+        }
     }
 
     @FXML
-    protected void register(ActionEvent actionEvent) throws Exception {
-        String username = usernameTextField.getText();
-        String password = passwordTextField.getText();
-        String verifyPassword = verifyPasswordTextField.getText();
-        String email = emailTextField.getText();
-        String type = userTypeField.getValue();
-        RegisterBean bean = new RegisterBean(email, username, type, password, verifyPassword);
+    protected void register(ActionEvent actionEvent) {
+        try {
+            String username = usernameTextField.getText();
+            String password = passwordTextField.getText();
+            String verifyPassword = verifyPasswordTextField.getText();
+            String email = emailTextField.getText();
+            String type = userTypeField.getValue();
+            RegisterBean bean = new RegisterBean(email, username, type, password, verifyPassword);
 
-        controller = new RegisterController();
-        User user = controller.register(bean);
-        LoggedInUser.getInstance().setUser(user);
-        if (user instanceof Client) {
-            FXMLLoader fxmlLoader = new FXMLLoader(ShoppingPointApplication.class.getResource("searchstore.fxml"));
-            ((Node) actionEvent.getSource()).getScene().setRoot(fxmlLoader.load());
-        }
-        if (user instanceof StoreOwner) {
-            FXMLLoader fxmlLoader = new FXMLLoader(ShoppingPointApplication.class.getResource("new_store.fxml"));
-            Parent node = fxmlLoader.load();
-            NewStoreGraphicController newStoreController = fxmlLoader.getController();
-            newStoreController.setStoreOwnerName((StoreOwner) user);
-            ((Node) actionEvent.getSource()).getScene().setRoot(node);
-        }
-        if (user instanceof Supplier) {
-            FXMLLoader fxmlLoader = new FXMLLoader(ShoppingPointApplication.class.getResource("request_list.fxml"));
-            Parent node = fxmlLoader.load();
-            ((Node) actionEvent.getSource()).getScene().setRoot(node);
-            RequestListGraphicController requestListGraphicControllerController = fxmlLoader.getController();
-            requestListGraphicControllerController.initialize();
+            RegisterController controller = new RegisterController();
+            User user = controller.register(bean);
+            LoggedInUser.getInstance().setUser(user);
+            if (user instanceof Client) {
+                FXMLLoader fxmlLoader = new FXMLLoader(ShoppingPointApplication.class.getResource("searchstore.fxml"));
+                ((Node) actionEvent.getSource()).getScene().setRoot(fxmlLoader.load());
+            }
+            if (user instanceof StoreOwner) {
+                FXMLLoader fxmlLoader = new FXMLLoader(ShoppingPointApplication.class.getResource("new_store.fxml"));
+                Parent node = fxmlLoader.load();
+                NewStoreGraphicController newStoreController = fxmlLoader.getController();
+                newStoreController.setStoreOwnerName((StoreOwner) user);
+                ((Node) actionEvent.getSource()).getScene().setRoot(node);
+            }
+            if (user instanceof Supplier) {
+                FXMLLoader fxmlLoader = new FXMLLoader(ShoppingPointApplication.class.getResource("request_list.fxml"));
+                Parent node = fxmlLoader.load();
+                ((Node) actionEvent.getSource()).getScene().setRoot(node);
+                RequestListGraphicController requestListGraphicControllerController = fxmlLoader.getController();
+                requestListGraphicControllerController.initialize();
+            }
+        } catch (BeanException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("Incorrect data");
+            alert.setContentText(e.getMessage());
+            alert.show();
+        } catch (IOException ioException) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("Load error");
+            alert.setContentText("Could not load the next screen. Please try again");
+            alert.show();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 }
