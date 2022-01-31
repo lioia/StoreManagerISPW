d9f668c4b1ddd6be317da8698bca76ecbb222b0d
Alessandro Lioi <lioialessandro@users.noreply.github.com>
2022-01-31 12:47:43 +0100

    Close #58

--- ShoppingPoint/src/main/java/com/example/shoppingpoint/bean/RequestListBean.java
+++ ShoppingPoint/src/main/java/com/example/shoppingpoint/bean/RequestListBean.java
@@ -1,18 +1,22 @@
 package com.example.shoppingpoint.bean;
 
+import com.example.shoppingpoint.exception.BeanException;
+
 public class RequestListBean {
     private float offerPrice;
-    public RequestListBean(String offerPrice)throws Exception{
+
+    public RequestListBean(String offerPrice) throws BeanException {
         setOfferPrice(offerPrice);
     }
 
-    public void setOfferPrice(String offerPrice) throws Exception{
-        float offerPrice1=Float.parseFloat(offerPrice);
-        if(offerPrice1 < 0 ) {
-//            TODO exception
-            System.out.println("Invalid data");
+    public void setOfferPrice(String offerPrice) throws BeanException {
+        try {
+            float offer = Float.parseFloat(offerPrice);
+            if(offer < 0) throw new BeanException("offer price", "it has to be more than 0");
+            this.offerPrice = offer;
+        } catch(NumberFormatException e) {
+            throw new BeanException("offer price", "invalid format");
         }
-        this.offerPrice = offerPrice1;
     }
 
     public float getOfferPrice() {
--- ShoppingPoint/src/main/java/com/example/shoppingpoint/view/RequestListGraphicController.java
+++ ShoppingPoint/src/main/java/com/example/shoppingpoint/view/RequestListGraphicController.java
@@ -1,17 +1,15 @@
 package com.example.shoppingpoint.view;
 
 import com.example.shoppingpoint.ShoppingPointApplication;
-import com.example.shoppingpoint.model.user.Supplier;
+import com.example.shoppingpoint.adapter.GenericProduct;
+import com.example.shoppingpoint.exception.BeanException;
 import com.example.shoppingpoint.singleton.LoggedInUser;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import com.example.shoppingpoint.controller.RequestListController;
@@ -23,7 +21,6 @@ import java.util.List;
 
 import com.example.shoppingpoint.model.Request;
 import javafx.scene.text.Text;
-import javafx.scene.control.Label;
 import org.controlsfx.control.PopOver;
 
 public class RequestListGraphicController {
@@ -35,50 +32,55 @@ public class RequestListGraphicController {
         createRequestPaneView();
     }
 
-    private void createRequestPaneView() throws Exception {
-        requestPane.getChildren().clear();
-        RequestListController controller = new RequestListController();
-        List<Request> requestList = controller.getRequest();
-        for (Request request : requestList) {
-            FXMLLoader fxmlLoader = new FXMLLoader(ShoppingPointApplication.class.getResource("reusable/request_product_pane.fxml"));
-            AnchorPane node = fxmlLoader.load();
-            //TODO è giusto cosi product adapter?
-            ProductAdapter product = new ProductAdapter(controller.getProduct(request.getProductId()));
+    private void createRequestPaneView() {
+        try {
+            requestPane.getChildren().clear();
+            RequestListController controller = new RequestListController();
+            List<Request> requestList = controller.getRequest();
+            for (Request request : requestList) {
+                FXMLLoader fxmlLoader = new FXMLLoader(ShoppingPointApplication.class.getResource("reusable/request_product_pane.fxml"));
+                AnchorPane node = fxmlLoader.load();
+                GenericProduct product = new ProductAdapter(controller.getProduct(request.getProductId()));
 
-            ((Text) node.lookup("#ProductName")).setText(product.getName());
-            ((Text) node.lookup("#quantity")).setText(String.format("Quantity: %d", request.getQuantity()));
-            ((Text) node.lookup("#maxPrice")).setText(String.format("Max Price:%.2f", request.getMaxPrice()));
-            ((Label) node.lookup("#description")).setText(product.getDescription());
-            ((Text) node.lookup("#store")).setText("Store: "+product.getStore());
-            ((Button) node.lookup("#descriptionButton")).setOnAction((ActionEvent event) -> {
-                ScrollPane scrollPane = new ScrollPane();
-                scrollPane.setMaxWidth(400.0);
-                scrollPane.setMaxHeight(400.0);
-                scrollPane.setPadding(new Insets(16));
-                Label label = new Label();
-                label.setText(product.getDescription());
-                label.setStyle("-fx-font-size: 16px");
-                label.setMaxWidth(350.0);
-                label.setWrapText(true);
-                scrollPane.setContent(label);
-                PopOver popOver = new PopOver();
-                Node pane = (Node) event.getSource();
-                popOver.setArrowLocation(PopOver.ArrowLocation.TOP_LEFT);
-                popOver.setContentNode(scrollPane);
-                popOver.setCornerRadius(16);
-                popOver.show(pane);
-            });
-            ((Button) node.lookup("#offerButton")).setOnAction((ActionEvent event)  -> {
-                try {
-                    controller.saveOffer(product.getId(), new RequestListBean( ((TextField) node.lookup("#choosePriceTextField")).getText()));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
-
-
-
-            requestPane.getChildren().add(node);
+                ((Text) node.lookup("#ProductName")).setText(product.getName());
+                ((Text) node.lookup("#quantity")).setText(String.format("Quantity: %d", request.getQuantity()));
+                ((Text) node.lookup("#maxPrice")).setText(String.format("Max Price:%.2f", request.getMaxPrice()));
+                ((Label) node.lookup("#description")).setText(product.getDescription());
+                ((Text) node.lookup("#store")).setText("Store: " + product.getStore());
+                ((Button) node.lookup("#descriptionButton")).setOnAction((ActionEvent event) -> {
+                    ScrollPane scrollPane = new ScrollPane();
+                    scrollPane.setMaxWidth(400.0);
+                    scrollPane.setMaxHeight(400.0);
+                    scrollPane.setPadding(new Insets(16));
+                    Label label = new Label();
+                    label.setText(product.getDescription());
+                    label.setStyle("-fx-font-size: 16px");
+                    label.setMaxWidth(350.0);
+                    label.setWrapText(true);
+                    scrollPane.setContent(label);
+                    PopOver popOver = new PopOver();
+                    Node pane = (Node) event.getSource();
+                    popOver.setArrowLocation(PopOver.ArrowLocation.TOP_LEFT);
+                    popOver.setContentNode(scrollPane);
+                    popOver.setCornerRadius(16);
+                    popOver.show(pane);
+                });
+                ((Button) node.lookup("#offerButton")).setOnAction((ActionEvent event) -> {
+                    try {
+                        controller.saveOffer(product.getId(), new RequestListBean(((TextField) node.lookup("#choosePriceTextField")).getText()));
+                    } catch (BeanException e) {
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setHeaderText("Incorrect data");
+                        alert.setContentText(e.getMessage());
+                        alert.show();
+                    } catch (Exception e) { // TODO handle controller exception
+                        e.printStackTrace();
+                    }
+                });
+                requestPane.getChildren().add(node);
+            }
+        } catch (Exception e) { // TODO handle controller exception
+            e.printStackTrace();
         }
     }
 
